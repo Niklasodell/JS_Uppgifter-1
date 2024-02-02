6b459c858a45503f432d8f7e32fab62104283546
Niklasodell <144327353+Niklasodell@users.noreply.github.com>
2024-02-02 09:02:18 +0100

    Initial commit

---
+++ switch.js
@@ -0,0 +1,15 @@
+const day = "Monday"
+
+switch (day) { 
+    case "Friday": 
+        console.log("Fredaaaag, ut och festa!")
+        break
+    case "Saturday": 
+        console.log("Lördag, ta det lugnt hemma")
+        break
+    case "Sunday": 
+        console.log("Söndag, förberada för skolan imorgon")
+        break
+    default: 
+        console.log("Mitt i veckan...")
+}
